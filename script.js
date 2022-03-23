7226dac6e9fd4afeabc886137b82e41ab72d94ed
Justas <justas.pnt@gmail.com>
2022-03-23 14:27:57 +0200

    added playRound function

--- script.js
+++ script.js
@@ -2,10 +2,39 @@
 
 const choices = ["rock", "paper", "scissors"];
 
-const computerPlay = function (choices, numberOfChoices) {
-  let choice = choices[Math.floor(Math.random() * numberOfChoices)];
+/* computerPlay */
+const computerPlay = function () {
+  let choice = choices[Math.floor(Math.random() * 3)];
 
   return choice;
 };
 
-console.log(computerPlay(choices, 3));
+/* playRound */
+const playRound = function (playerSel, computerSelection) {
+  let playerSelection = playerSel.toLowerCase();
+
+  if (!choices.includes(playerSelection)) {
+    return "Unavailable choice";
+  }
+
+  if (playerSelection === computerSelection) {
+    return `Tie`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `You win! rock beats scissors`;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return `You lose! rock beats scissors`;
+  } else if (
+    choices.indexOf(playerSelection) > choices.indexOf(computerSelection)
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+  }
+};
+
+let playerSelection = "Scissors";
+let computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
+
+// console.log(computerPlay(choices, 3));
