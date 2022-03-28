dff1dd5e235b9d0aef634b0e2d66183cf0d18718
Justas <justas.pnt@gmail.com>
2022-03-28 03:55:23 +0300

    updated ui

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ const choiceBtns = document.querySelectorAll(".choiceBtn");
 const roundsLog = document.querySelector("#roundsLog");
 const resultDiv = document.querySelector("#gameResult");
 const winsCounterP = document.querySelector("#winsCounter");
-const resultP = document.createElement("p");
+const playerSelectionDiv = document.querySelectorAll(".selection")[0];
+const computerSelectionDiv = document.querySelectorAll(".selection")[1];
+const restartBtn = document.createElement("button");
+restartBtn.type = "button";
+restartBtn.classList.add("restartBtn");
+restartBtn.textContent = "Play Again";
 
 /* Helper functions */
 const firstLetterUpperCase = function (word) {
@@ -25,11 +30,11 @@ const computerPlay = function () {
 
 /* Rock paper scissors game logic */
 const playRound = function () {
-  let playerSelection = this.name;
+  let playerSelection = this.alt;
   let computerSelection = computerPlay();
 
-  console.log(playerSelection);
-  console.log(computerSelection);
+  playerSelectionDiv.innerHTML = `<img class="image"  src="images/${playerSelection}.png" alt="${playerSelection}" />`;
+  computerSelectionDiv.innerHTML = `<img class="image" src="images/${computerSelection}.png" alt="${computerSelection}" />`;
 
   if (playerSelection === computerSelection) {
     roundsLog.textContent = "It's a Tie!";
@@ -58,20 +63,36 @@ const game = function (e) {
 
   if (winner === "player") playerWins++;
   if (winner === "computer") computerWins++;
-  winsCounterP.textContent = `Player: ${playerWins} | Computer ${computerWins}`;
+  winsCounterP.innerHTML = `<span>Player: ${playerWins}</span> <span>Computer: ${computerWins}</span>`;
 
   if (playerWins === 5 || computerWins === 5) {
-    choiceBtns.forEach((btn) => (btn.disabled = true));
-    playerWins === 5
-      ? (resultP.textContent = "You won the game. Reload the page to play again.")
-      : (resultP.textContent = "You lost the game. Reload the page to play again.");
+    playerWins === 5 ? (roundsLog.textContent = `You won.`) : (roundsLog.textContent = `You lost.`);
+    roundsLog.appendChild(restartBtn);
 
-    resultDiv.insertBefore(resultP, winsCounterP);
+    choiceBtns.forEach((btn) => {
+      btn.removeEventListener("click", game);
+    });
   }
 };
 
-/* adding event listiners to rock paper scissors buttons */
+/* restart game */
+const restartGame = function () {
+  winner = "";
+  playerWins = 0;
+  computerWins = 0;
+  playerSelectionDiv.innerHTML = "";
+  computerSelectionDiv.innerHTML = "";
+  roundsLog.textContent = "";
+  winsCounterP.innerHTML = `<span>Player: 0</span> <span>Computer: 0</span>`;
 
+  choiceBtns.forEach(function (btn) {
+    btn.addEventListener("click", game);
+  });
+};
+
+/* event listeners*/
 choiceBtns.forEach(function (btn) {
   btn.addEventListener("click", game);
 });
+
+restartBtn.addEventListener("click", restartGame);
